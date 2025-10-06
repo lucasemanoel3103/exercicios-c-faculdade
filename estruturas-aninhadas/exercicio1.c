7c9e018cbfe59230487c57d6fcaf0ae4321acd41
Lucas emanoel <lucasemanoel625@gmail.com>
2025-10-06 19:44:59 -0300

    exercicios de C

---
+++ estruturas-aninhadas/exercicio1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(){
+    int idade;
+    float renda;
+
+   printf("Digite a sua idade: ");
+   scanf("%d", &idade);
+   printf("Digite a sua renda mensal: ");
+   scanf("%f", &renda);
+   
+   if(idade <= 18 || idade >= 60)
+   {
+    
+   }
+}
